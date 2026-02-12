0b1f1e3247ac29e891510761d783ad973cbb7799
koushik6336 <136243534+koushik6336@users.noreply.github.com>
2026-02-12 17:57:43 +0530

    Add AWS Lambda functions for data processing and reporting

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,3 +1,18 @@
+resource "aws_lambda_function" "data_processor" {
+  function_name = "data_processor_lambda"
+  runtime       = "python3.9"
+  handler       = "processor.lambda_handler"
+  role          = aws_iam_role.lambda_role.arn
+  filename      = "../lambda/processor.zip"
+}
+
+resource "aws_lambda_function" "daily_report" {
+  function_name = "daily_report_lambda"
+  runtime       = "python3.9"
+  handler       = "report.lambda_handler"
+  role          = aws_iam_role.lambda_role.arn
+  filename      = "../lambda/report.zip"
+}
 resource "aws_lambda_permission" "allow_s3" {
   statement_id  = "AllowS3Invoke"
   action        = "lambda:InvokeFunction"

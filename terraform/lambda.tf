84b10ea7fa80347f331f0cc30e5bad80ee70d1fa
koushik6336 <136243534+koushik6336@users.noreply.github.com>
2026-02-12 17:53:47 +0530

    Add S3 trigger and permissions for data processor

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,15 +1,18 @@
-resource "aws_lambda_function" "data_processor" {
-  function_name = "data_processor_lambda"
-  runtime       = "python3.9"
-  handler       = "processor.lambda_handler"
-  role          = aws_iam_role.lambda_role.arn
-  filename      = "../lambda/processor.zip"
+resource "aws_lambda_permission" "allow_s3" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.data_processor.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.raw_bucket.arn
 }
 
-resource "aws_lambda_function" "daily_report" {
-  function_name = "daily_report_lambda"
-  runtime       = "python3.9"
-  handler       = "report.lambda_handler"
-  role          = aws_iam_role.lambda_role.arn
-  filename      = "../lambda/report.zip"
+resource "aws_s3_bucket_notification" "raw_trigger" {
+  bucket = aws_s3_bucket.raw_bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.data_processor.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+
+  depends_on = [aws_lambda_permission.allow_s3]
 }
